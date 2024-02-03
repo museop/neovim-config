7296b7d6f61856548dfddf99a0abb7b8f93202a1
museop <museop92@gmail.com>
2024-02-04 01:02:04 +0900

    feat: add colorschemes and change default

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -18,9 +18,23 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
+        -- config = function()
+        --     vim.cmd("colorscheme rose-pine")
+        -- end
+    },
+    {
+        'AlexvZyl/nordic.nvim',
+        lazy = false,
+        priority = 1000,
         config = function()
-
-            vim.cmd("colorscheme rose-pine")
+            require 'nordic'.load()
+            vim.cmd("colorscheme nordic")
         end
     },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    }
+
 }
